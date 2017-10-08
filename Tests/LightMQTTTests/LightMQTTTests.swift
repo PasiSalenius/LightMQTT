b7359f55610f8f628fd71ee35700e5aef942f2ba
Luis de la Rosa <luis@luisdelarosa.com>
2017-10-07 22:16:25 -0400

    Create simple test

--- Tests/LightMQTTTests/LightMQTTTests.swift
+++ Tests/LightMQTTTests/LightMQTTTests.swift
@@ -2,14 +2,14 @@ import XCTest
 @testable import LightMQTT
 
 class LightMQTTTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(LightMQTT().text, "Hello, World!")
+    func testNotNil() {
+        let host = "mqtt.foobar.com"
+        let instance = LightMQTT(host: host)
+        XCTAssertNotNil(instance)
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testNotNil", testNotNil),
     ]
 }
